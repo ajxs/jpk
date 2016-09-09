7aa895f171c298f31984499dfbee63aa02a8382e
ajxs <ajxscc@gmail.com>
2016-09-09 14:24:09 +1000

    Efficiency improvements

--- main.js
+++ main.js
@@ -20,33 +20,17 @@ var debug = {
 	bitcrush: {
 		nSegments: 0,
 		nDistortions: 0
-	}
-};
-
-var controlInputElements = {
-	distort: {
-		intervalMin: null,
-		intervalMax: null,
-		threshold: null
-	},
-	slicer: {
-		iterations: null
 	},
 	delay: {
-
-	},
-	bitcrush: {
-		enabled: null,
-		thresold: null,
-		interval: null
+		segSource: 0,
+		segTarget: 0
 	}
 };
-
 
 var controlValues = {
 	distort_interval_min: 50,
 	distort_interval_max: 80,
-	distort_threshold: 10,
+	distort_threshold: 1,
 	slicer_seg_length: 75,
 	slicer_iterations: 25,
 	control7: 15,
@@ -63,9 +47,6 @@ function main_init() {
 	srcContext = srcCanvas.getContext('2d');
 	mainContext = mainCanvas.getContext('2d');
 
-	initControlItems();
-
-	getControlValues();
 	loadFileFromImg(document.getElementById('img_5'));
 };
 
@@ -111,56 +92,31 @@ function loadFileFromImg(img) {
 };
 
 
-
-function initControlItems() {
-	controlInputElements.distort.intervalMin = document.getElementById('control_distort_interval_min');
-	controlInputElements.distort.intervalMax = document.getElementById('control_distort_interval_max');
-	controlInputElements.distort.threshold = document.getElementById('control_distort_threshold');
-
-	controlInputElements.slicer.iterations = document.getElementById('control_slicer_iterations');
-
-	controlInputElements.bitcrush.enabled = document.getElementById('control_bitcrush');
-	controlInputElements.bitcrush.threshold = document.getElementById('control_bitcrush_threshold');
-	controlInputElements.bitcrush.interval = document.getElementById('control_bitcrush_interval');
-};
-
 function getControlValues() {
-	controlValues.distort_interval_min = controlInputElements.distort.intervalMin.value;
-	document.getElementById('control_1_value').innerHTML = controlInputElements.distort.intervalMin.value;
-
-	controlValues.distort_interval_max = controlInputElements.distort.intervalMax.value;
-	document.getElementById('control_2_value').innerHTML = controlInputElements.distort.intervalMax.value;
-
-	controlValues.distort_threshold = controlInputElements.distort.threshold.value;
-	document.getElementById('control_3_value').innerHTML = controlInputElements.distort.threshold.value;
-
+	controlValues.distort_interval_min = document.getElementById('control_distort_interval_min').value;
+	controlValues.distort_interval_max = document.getElementById('control_distort_interval_max').value;
+	controlValues.distort_threshold = document.getElementById('control_distort_threshold').value;
 	controlValues.slicer_seg_length = document.getElementById('control_4').value;
-	document.getElementById('control_4_value').innerHTML = document.getElementById('control_4').value;
-
-	controlValues.slicer_iterations = controlInputElements.slicer.iterations.value;
-	document.getElementById('control_6_value').innerHTML = controlInputElements.slicer.iterations.value;
-
+	controlValues.slicer_iterations = document.getElementById('control_slicer_iterations').value;
 	controlValues.control7 = document.getElementById('control_7').value;
-	document.getElementById('control_7_value').innerHTML = document.getElementById('control_7').value;
-
 	controlValues.control8 = document.getElementById('control_8').value;
-	document.getElementById('control_8_value').innerHTML = document.getElementById('control_8').value;
-
-	controlValues.bitcrush = controlInputElements.bitcrush.enabled.checked;
-
-	controlValues.bitcrush_threshold = controlInputElements.bitcrush.threshold.value;
-	document.getElementById('control_bitcrush_threshold_value').innerHTML = controlInputElements.bitcrush.threshold.value;
-
-	controlValues.bitcrush_interval = controlInputElements.bitcrush.interval.value;
-	document.getElementById('control_bitcrush_interval_value').innerHTML = controlInputElements.bitcrush.interval.value;
-
+	controlValues.bitcrush = document.getElementById('control_bitcrush').checked;
+	controlValues.bitcrush_threshold = document.getElementById('control_bitcrush_threshold').value;
+	controlValues.bitcrush_interval = document.getElementById('control_bitcrush_interval').value;
 };
+
+
+function updateControlLabel(node) {
+	document.getElementById(node.id + '_value').innerHTML = node.value;
+};
+
 
 
 var segSource, segTarget, segLength;
 var sourceData, targetData;
 
 function crush() {
+	getControlValues();
 	var dataURL = srcCanvas.toDataURL("image/jpeg", 1.0),
 		img_byteArray = base64toBinary(dataURL);
 
@@ -170,18 +126,20 @@ function crush() {
 	mainContext.globalCompositeOperation = 'source-over';
 	mainContext.globalAlpha = 1;
 
-	while(img_ptr < img_byteArray.length) {
-		img_ptr += controlValues.distort_interval_min+(Math.random()*(controlValues.distort_interval_max))|0;
-		debug.distort.nSegments++;
 
-		if(Math.random() < (controlValues.distort_threshold/100)) {
-			img_byteArray[img_ptr] = (Math.random() * 255)|0;
-			debug.distort.nDistortions++;
+	var distort = function() {
+		while(img_ptr < img_byteArray.length) {
+			img_ptr += controlValues.distort_interval_min+(Math.random()*(controlValues.distort_interval_max))|0;
+			debug.distort.nSegments++;
+
+			if(Math.random() < (controlValues.distort_threshold/10)) {
+				img_byteArray[img_ptr] = (Math.random() * 255)|0;
+				debug.distort.nDistortions++;
+			}
 		}
+	};
 
-	}
-
-	if(controlValues.slicer_iterations > 0) {		// SLICER
+	var slicer = function() {
 		for(var c = 0; c < (controlValues.slicer_iterations)|0; c++) {
 			segLength = (Math.random()*50*controlValues.slicer_seg_length)|0;
 
@@ -201,9 +159,10 @@ function crush() {
 				img_byteArray[segTarget + i] = sourceData[segSource + i];
 			}
 		}
-	}
+	};
 
-	if(controlValues.bitcrush) {
+
+	var bitcrush = function() {
 		img_ptr = img_headerLength;
 		while(img_ptr < img_byteArray.length) {
 			img_ptr += (controlValues.bitcrush_interval*(Math.random()*20))|0;
@@ -213,40 +172,46 @@ function crush() {
 				debug.bitcrush.nDistortions++;
 			}
 		}
-	}
+	};
 
-	var newImage = binaryToBase64Img(img_byteArray);
-	var img_byteArray_slicer = base64toBinary(dataURL);
-	newImage.onload = function() {		// fixes issues with Firefox
-		mainContext.drawImage(newImage, 0, 0);
+	var delay = function() {
+		var img_byteArray_delay = base64toBinary(dataURL);
+		segLength = (Math.random()*100*controlValues.control8)|0;
 
-		segLength = (Math.random()*100*controlValues.control8)|0;		// DELAY
+		segSource = img_byteArray_delay.length;
+		while(segSource + segLength > img_byteArray_delay.length) segSource = (Math.random()*img_byteArray_delay.length)|0;
+		debug.delay.segSource = segSource;
 
-		segSource = img_byteArray_slicer.length;
-		while(segSource + segLength > img_byteArray_slicer.length) segSource = (Math.random()*img_byteArray_slicer.length)|0;
+		segTarget = img_byteArray_delay.length;
+		while(segTarget + segLength > img_byteArray_delay.length) segTarget = (Math.random()*img_byteArray_delay.length)|0;
+		debug.delay.segTarget = segSource;
 
-		segTarget = img_byteArray_slicer.length;
-		while(segTarget + segLength > img_byteArray_slicer.length) segTarget = (Math.random()*img_byteArray_slicer.length)|0;
-
-		sourceData = new Uint8ClampedArray(segLength);
-		targetData = new Uint8ClampedArray(segLength);
+		sourceData = img_byteArray_delay.subarray(segSource, segSource+segLength);
+		targetData = img_byteArray_delay.subarray(segTarget, segTarget+segLength);
 
 		for(var i = 0; i < segLength; i++) {
-			sourceData[i] = img_byteArray_slicer[i + segSource];
-			targetData[i] = img_byteArray_slicer[i + segTarget];
-
-			img_byteArray_slicer[segSource + i] = targetData[segTarget + i];
-			img_byteArray_slicer[segTarget + i] = sourceData[segSource + i];
+			img_byteArray_delay[segSource + i] = targetData[segTarget + i];
+			img_byteArray_delay[segTarget + i] = sourceData[segSource + i];
 		}
 
-		var newImage2 = binaryToBase64Img(img_byteArray_slicer);
-		newImage2.onload = function() {
+		var imageResult_delay = binaryToBase64Img(img_byteArray_delay);
+		imageResult_delay.onload = function() {
 			mainContext.globalCompositeOperation = 'source-over';
 			mainContext.globalAlpha = (controlValues.control7/200);
-			mainContext.drawImage(newImage2, 0, 0);
+			mainContext.drawImage(imageResult_delay, 0, 0);
 		};
-
 	};
+
+
+	distort();
+	if(controlValues.slicer_iterations > 0) slicer();
+	if(controlValues.bitcrush) bitcrush();
+
+	var imageResult = binaryToBase64Img(img_byteArray);
+	imageResult.onload = function() {
+		mainContext.drawImage(imageResult, 0, 0);
+		delay();
+	}
 
 };
 
@@ -262,9 +227,10 @@ function jpeg_getHeaderLength(_byteArray) {
 
 function binaryToBase64Img(srcArray) {
 	var img = new Image();
-	img.src = "data:image/jpeg;base64," + window.btoa(Array.prototype.map.call(srcArray, function(x) {	//encode binary to ascii
+	var data = window.btoa(Array.prototype.map.call(srcArray, function(x) {	//encode binary to ascii
 		return String.fromCharCode(x);
 	}).join(''));
+	img.src = "data:image/jpeg;base64," +  data;
 	return img;
 };
 
